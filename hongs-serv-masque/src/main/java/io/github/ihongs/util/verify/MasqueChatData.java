5b72b610a2d96a153f384db3a30ef6cc21a538ec
Kevin Hongs <kevin.hongs@gmail.com>
2019-01-18 11:00:29 +0800

    重构校验参数

--- hongs-serv-masque/src/main/java/io/github/ihongs/util/verify/MasqueChatData.java
+++ hongs-serv-masque/src/main/java/io/github/ihongs/util/verify/MasqueChatData.java
@@ -15,10 +15,11 @@ import java.util.Map;
 public class MasqueChatData extends Rule {
 
     @Override
-    public Object verify(Object value, Wheel watch) throws Wrong {
-        Map  values = watch.getValues();
-        String  id  = Synt.declare(values.get( "id" ), ""/**/);
-        String kind = Synt.declare(values.get("kind"), "text");
+    public Object verify(Value watch) throws Wrong {
+        Object val  = watch.get();
+        Map    vals = watch.getValues();
+        String  id  = Synt.declare(vals.get( "id" ), ""/**/);
+        String kind = Synt.declare(vals.get("kind"), "text");
         switch(kind) {
             case "image":
             {
@@ -33,7 +34,7 @@ public class MasqueChatData extends Rule {
 
                 // 取缩略图
                 try {
-                    io.github.ihongs.util.sketch.Thumb th = new io.github.ihongs.util.sketch.Thumb(Core.BASE_PATH+"/static/upload/tmp/"+value);
+                    io.github.ihongs.util.sketch.Thumb th = new io.github.ihongs.util.sketch.Thumb(Core.BASE_PATH+"/static/upload/tmp/"+val);
                     th.make().scale(1).outputFormat("png").toFile(path +    ".png");
                     th.pick(300 , 300).outputFormat("png").toFile(path + "_sm.png");
                 } catch (IOException ex) {
@@ -60,7 +61,7 @@ public class MasqueChatData extends Rule {
                 }
 
                 // 转移文件
-                name = (String) value;
+                name = (String) val;
                 int pos  = name.lastIndexOf(".");
                 if (pos != -1 ) {
                     name = name.substring  (pos);
@@ -68,7 +69,7 @@ public class MasqueChatData extends Rule {
                     name =  "" ;
                 }
                 File df = new File (path + name);
-                File sf = new File (Core.BASE_PATH + "/static/upload/tmp/" + value);
+                File sf = new File (Core.BASE_PATH + "/static/upload/tmp/" + val);
                 sf.renameTo(df);
 
                 return Data.toString(Synt.mapOf(
@@ -78,12 +79,11 @@ public class MasqueChatData extends Rule {
                 ) );
             }
             case "link" :
-                if (value instanceof Map) {
-                    return  value;
+                if (val instanceof Map) {
+                    return  val;
                 }
 
-                return Data.toString(Synt.mapOf(
-                    "href", value,
+                return Data.toString(Synt.mapOf("href", val,
                     "snap", "",
                     "desc", ""
                 ) );
